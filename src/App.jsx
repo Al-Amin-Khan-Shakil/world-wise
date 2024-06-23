530b8722ef097db7e668ab6e223b4ca36320c02f
Al-Amin <alaminkhanshakil@gmail.com>
2024-06-23 23:09:03 +0600

    Install vite with react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState();
+  return (
+    <div>
+      <h1>Hello Vite!!!</h1>
+    </div>
+  );
+}
+
+export default App;
