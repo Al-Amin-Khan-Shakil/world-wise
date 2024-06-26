efce7adf27a02de816d08eeafb1b1b1aaccb8fdf
Al-Amin <alaminkhanshakil@gmail.com>
2024-06-26 12:25:54 +0600

    Import starter files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,15 +5,15 @@ export default function Homepage() {
     <main className={styles.homepage}>
       <section>
         <h1>
-          You trvel the world.
+          You travel the world.
           <br />
           WorldWise keeps track of your adventures.
         </h1>
-        <p>
-          A world map that tracks your footsetps into every city you can think
+        <h2>
+          A world map that tracks your footsteps into every city you can think
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
-        </p>
+        </h2>
       </section>
     </main>
   );
