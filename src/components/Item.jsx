f97f06d9ac6069615d333926f73ff812fd86a198
Al-Amin <alaminkhanshakil@gmail.com>
2024-07-04 21:17:13 +0600

    Styled the item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,11 @@
 import { useTravelBag } from "../contexts/TravelBagContext";
+import styles from "./Item.module.css";
 
 export default function Item({ item }) {
   const { handleToggleItems, handleDeleteItem } = useTravelBag();
 
   return (
-    <li>
+    <li className={styles.listItem}>
       <input
         type="checkbox"
         onChange={() => handleToggleItems(item.id)}
